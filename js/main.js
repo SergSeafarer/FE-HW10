d845c89fdb18278f3dd34102321a065d7915422d
Sergii <seregaonma@gmail.com>
2023-10-12 01:34:32 +0300

    complete second variant of task

--- js/main.js
+++ js/main.js
@@ -36,3 +36,32 @@ if(arrLength === null) {
     document.write('<br>' + 'Final array: ' + finalArray);
   } 
 }
+
+/* Second variant */
+
+/*let arrLength = prompt('Введіть будь-ласка довжину масиву більше 4 та менше 10');
+if(arrLength === null) {
+  alert('Шкода');
+} else {
+  while(isNaN(+arrLength) || +arrLength < 4 || +arrLength > 10 || +arrLength === 0 || arrLength === '') {
+    arrLength = +prompt('Введіть будь-ласка довжину масиву більше 4 та менше 10');
+  }
+  let arrContent = prompt('Введіть числа через пробіл');
+  if(arrContent === null) {
+    alert('Шкода');
+  } else {
+    while(isNaN(Number.parseInt(arrContent)) || arrContent === '' ) {
+      alert('Будь-ласка введіть числа');
+      arrContent = prompt('Введіть числа через пробіл');
+    }
+    const initialArray = arrContent.split(' ');
+    document.write('Initial array: ' + initialArray);
+    const sortedArray = [...initialArray].sort(function(a, b) {
+      return a-b;
+    })
+    document.write('<br>' + 'Sorted array: ' + sortedArray);
+    const finalArray = [...sortedArray];
+    finalArray.splice(1, 3);
+    document.write('<br>' + 'Final Array: ' + finalArray);
+  }
+}*/
